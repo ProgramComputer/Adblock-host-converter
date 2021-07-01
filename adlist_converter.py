8c03538a8cc18bf357fb6eb60c0f56bba0e0f5c7
Paul <22284856+ProgramComputer@users.noreply.github.com>
2021-07-01 08:33:17 -0500

    Update adlist_converter.py

--- adlist_converter.py
+++ adlist_converter.py
@@ -98,7 +98,8 @@ def writeList(inURL, adList, newFile):
 	''' Create parsed blocklist file.
 	'''
 	csvStack = []
-	writer = open(newFile, 'w+') #create if not exist and write
+	os.makedirs(os.path.dirname(newFile), exist_ok=True)
+	writer = open(newFile, 'w')
 	#writer.write("# Converted by: https://github.com/ProgramComputer/Adblock-hosts-converter"+"\n")
 	#writer.write("# Blocklist: "+inURL+"\n")
 	for line in adList.splitlines():
